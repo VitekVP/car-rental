eca6d31d2afb240e5a36cca9bc2aac1a1e0a73f4
Viktor Pochynok <viktor83pochynok@gmail.com>
2023-09-16 21:33:01 +0300

    add cardCar

--- src/components/CardCar/CardCar.jsx
+++ src/components/CardCar/CardCar.jsx
@@ -1,15 +1,49 @@
+// import { AiFillHeart } from 'react-icons/ai';
+import { AiOutlineHeart } from 'react-icons/ai';
+
 import styles from './CardCars.module.scss';
 
 const CardCar = ({ car }) => {
-  const { make, img } = car;
+  const {
+    make,
+    model,
+    img,
+    year,
+    rentalPrice,
+    address,
+    rentalCompany,
+    type,
+    mileage,
+    functionalities,
+  } = car;
 
   return (
     <li className={styles.card}>
       <div className={styles.thumb}>
         <img src={img} alt={make} />
       </div>
-      <div className={styles.info}></div>
-      <div className={styles.description}></div>
+      <AiOutlineHeart className={styles.icon} />
+      <ul className={styles.info}>
+        <li className={styles.infoItem}>
+          {make.slice(0, 9)} <span>{model.slice(0, 9)}</span>, {year}
+        </li>
+        <li className={styles.infoItem}>{rentalPrice}</li>
+      </ul>
+      <ul className={styles.location}>
+        <li className={styles.locationItem}>
+          {address.split(' ')[3].replace(',', '')}
+        </li>
+        <li className={styles.locationItem}>{address.split(' ').slice(-1)}</li>
+        <li className={styles.locationItem}>{rentalCompany}</li>
+      </ul>
+      <ul className={styles.description}>
+        <li className={styles.descriptionItem}>{type}</li>
+        <li className={styles.descriptionItem}>{model}</li>
+        <li className={styles.descriptionItem}>{mileage}</li>
+        <li className={styles.descriptionItem}>
+          {functionalities[0].slice(0, 21)}
+        </li>
+      </ul>
       <button type="button" className={styles.btn}>
         Learn more
       </button>
