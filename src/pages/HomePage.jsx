992cda4fcb4a08b31593594a0fd036511ce8cb77
Viktor Pochynok <viktor83pochynok@gmail.com>
2023-09-16 17:11:44 +0300

    change HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,31 @@
-import Content from 'components/Content/Content';
+import { Link } from 'react-router-dom';
+
+import Container from 'components/Shared/Container/Container';
+
+import styles from './HomePage.module.scss';
 
 const HomePage = () => {
   return (
-    <>
-      <Content />
-    </>
+    <section className={styles.wrapper}>
+      <Container>
+        <h1 className={styles.title}>Welcome to our car rental homepage!</h1>
+        <h2 className={styles.subtitle}>Car Rental, Your Way!</h2>
+        <p className={styles.text}>
+          Choose from a wide car selection. Flexible rental terms, no hidden
+          fees. Top-notch service, always ready to help.
+        </p>
+        <ul className={styles.list}>
+          <li className={styles.text}>1 - Pick your car.</li>
+          <li className={styles.text}>2 - Book online.</li>
+          <li className={styles.text}>3 - Get your car.</li>
+          <li className={styles.text}>4 - Enjoy your ride.</li>
+        </ul>
+        <h2 className={styles.subtitle}>Book now and hit the road with us!</h2>
+        <Link to="/catalog" className={styles.link}>
+          GO!
+        </Link>
+      </Container>
+    </section>
   );
 };
 
